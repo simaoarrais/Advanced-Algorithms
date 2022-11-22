ed285a3f67c98bf7a8d9817e5928bf3da304183d
simaoarrais <simaoarrais@gmail.com>
2022-11-22 15:34:50 +0000

    added basic documentation

--- Assignment2/solution.py
+++ Assignment2/solution.py
@@ -11,18 +11,24 @@ class Solution:
     def solution(self):
         rnd.seed(None)
         tested_candidates = list()
+
+        # Calculate how many vertices the candidate solution will have
         num_vertices_candidate = rnd.randint(2, len(self.vertices_dict.keys())) # candidate solutions must have at least 2 vertices
         print(f"num_vertices_candidate : {num_vertices_candidate}")
 
+        # Calculate what vertices will belong to the candidate solution
         candidate_set = set()
         for i in range(num_vertices_candidate):
             vertice_label = rnd.randint(0, len(self.vertices_dict.keys())-1)
             vertice = self.vertices_dict.get(vertice_label)
 
+            # TODO Random in a list of remaining vertices
+            # While the calculated vertex is already on the candidate solution, calculate a new one until it's not
             while (vertice in candidate_set):
                 vertice_label = rnd.randint(0, len(self.vertices_dict.keys())-1)
                 vertice = self.vertices_dict.get(vertice_label)
 
+            # Add vertex to candidate solution
             candidate_set.add(vertice)
 
         print(f"candidate -> {[v.label for v in candidate_set]}")
